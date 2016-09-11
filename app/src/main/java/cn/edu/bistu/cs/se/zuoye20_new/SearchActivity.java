2133671a4d86c73db965eb9c57ac9c1b5a04ee52
AKiraDDD <923771143@qq.com>
2016-09-11 13:38:07 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/zuoye20_new/SearchActivity.java
@@ -0,0 +1,48 @@
+package cn.edu.bistu.cs.se.zuoye20_new;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+             */
+
+        Intent intent=getIntent();
+        ArrayList<Map<String,String>> items = (ArrayList<Map<String,String>>) intent.getSerializableExtra("result");
+
+        SimpleAdapter adapter = new SimpleAdapter(this, items, R.layout.item,
+                new String[]{Words.Word._ID,Words.Word.COLUMN_NAME_WORD, Words.Word.COLUMN_NAME_MEANING, Words.Word.COLUMN_NAME_SAMPLE},
+                new int[]{R.id.textId,R.id.textViewWord, R.id.textViewMeaning, R.id.textViewSample});
+
+        ListView list = (ListView) findViewById(R.id.lstSearchResultWords);
+
+        list.setAdapter(adapter);
+
+
+
+
+    }
+
+}
